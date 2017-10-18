6c83cee1bf61a29ee1a1358f51ab5fde590d47ba
Kevin Yan <kevinyan215@gmail.com>
2017-10-17 17:42:49 -0700

    Power of three

---
+++ algorithms/isPowerOfThree.java
@@ -0,0 +1,22 @@
+package leetcode.algorithms;
+
+/**
+ * Created by kevinyan on 10/17/17.
+ */
+
+/*
+Given an integer, write a function to determine if it is a power of three.
+ */
+public class IsPowerOfThree{
+	 public boolean isPowerOfThree(int n) {
+        if(n <= 0) return false;
+        if(n == 1) return true;
+        int mod = 0;
+        while(n>=3 && n%3 ==0){
+            n = n/3;
+            if(n==1) return true;
+        }
+        
+        return false;
+    }
+}
